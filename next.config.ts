21b80e838fba5c80d99e901afc8b9df905ee1b75
Ahmed Shawky <shawkyahmed392@gmail.com>
2025-12-27 14:44:00 +0000

    the images is not showen when i deploy the site to GitHub Page but it wo

--- next.config.ts
+++ next.config.ts
@@ -2,83 +2,83 @@ import type { NextConfig } from 'next';
 
 const repoName = 'ShawkyDev';
 
-const nextConfig: NextConfig = {
-  output: 'export',
-
-  basePath: `/${repoName}`,
-  assetPrefix: `/${repoName}/`,
-
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
-  images: {
-    unoptimized: true, // REQUIRED for GitHub Pages
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'drive.google.com',
-        pathname: '/**',
-      },
-    ],
-  },
-};
-
-
 // const nextConfig: NextConfig = {
+//   output: 'export',
+
+//   basePath: `/${repoName}`,
+//   assetPrefix: `/${repoName}/`,
+
 //   typescript: {
 //     ignoreBuildErrors: true,
 //   },
+
 //   eslint: {
 //     ignoreDuringBuilds: true,
 //   },
+
 //   images: {
+//     unoptimized: true, // REQUIRED for GitHub Pages
 //     remotePatterns: [
 //       {
 //         protocol: 'https',
 //         hostname: 'placehold.co',
-//         port: '',
 //         pathname: '/**',
 //       },
 //       {
 //         protocol: 'https',
 //         hostname: 'images.unsplash.com',
-//         port: '',
 //         pathname: '/**',
 //       },
 //       {
 //         protocol: 'https',
 //         hostname: 'picsum.photos',
-//         port: '',
 //         pathname: '/**',
 //       },
 //       {
 //         protocol: 'https',
 //         hostname: 'drive.google.com',
-//         port: '',
 //         pathname: '/**',
 //       },
 //     ],
 //   },
 // };
+
+
+const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
